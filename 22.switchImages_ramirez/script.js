1d75b560555473ccf26acdbd036ed44fe6b79324
lrami328 <lramirez21cp@gmail.com>
2025-02-23 21:18:49 -0600

    Add files via upload

---
+++ 22.switchImages_ramirez/script.js
@@ -0,0 +1,16 @@
+
+const images = [
+	"images/image1.jpg", 
+	"images/image2.jpg", 
+	"images/image3.jpg"
+];
+let currentIndex = 0;
+
+const imageContainer = document.getElementById("imageContainer");
+imageContainer.style.backgroundImage = `url(${images[currentIndex]})`;
+
+document.body.addEventListener("click", () => {
+    currentIndex = (currentIndex + 1) % images.length;
+    imageContainer.style.backgroundImage = `url(${images[currentIndex]})`;
+});
+// JavaScript Document
